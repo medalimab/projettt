dbdf666d0be580288dfe8e7a110642e6b26c3f57
dali <mabroukdali1925@gmail.com>
2025-05-11 19:55:17 +0200

    feat(model): ajout du champ `year` dans le schéma Mongoose de Car

--- car-service/models/Car.js
+++ car-service/models/Car.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const carSchema = new mongoose.Schema({
   brand: String,
   model: String,
+  year: Number,
   available: { type: Boolean, default: true },
 });
 
